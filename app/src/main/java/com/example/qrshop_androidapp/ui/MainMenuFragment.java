20f4b28af4a25d5120f56261b8c9b6c440d2b95b
Explode <volodya.sokha@gmail.com>
2019-03-05 17:52:34 +0200

    OwerflowMainMenuAdd

--- app/src/main/java/com/example/qrshop_androidapp/ui/MainMenuFragment.java
+++ app/src/main/java/com/example/qrshop_androidapp/ui/MainMenuFragment.java
@@ -3,7 +3,11 @@ package com.example.qrshop_androidapp.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,6 +26,8 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         // INFLATING
 
         rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
@@ -29,5 +35,33 @@ public class MainMenuFragment extends Fragment {
         // INITIALIZATION
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.settingsMenuBtn:
+
+                //TODO
+                return true;
+            case R.id.cartMenuBtn:
+
+                //TODO
+                return true;
+            case R.id.exitMenuBtn:
+                //TODO
+                return true;
+            default:
+                break;
+        }
+
+        return false;
     }
 }
