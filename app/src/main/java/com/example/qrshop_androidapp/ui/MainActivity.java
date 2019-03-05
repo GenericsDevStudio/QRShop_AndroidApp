8be73be078d7bca3905fd35fbd400400e8a59b8a
und1f3n3d <47183753+und1f3n3d@users.noreply.github.com>
2019-03-05 20:40:07 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/qrshop_androidapp/ui/MainActivity.java
+++ app/src/main/java/com/example/qrshop_androidapp/ui/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import com.example.qrshop_androidapp.R;
 
 public class MainActivity extends AppCompatActivity {
-
+// TODO: RENAME DATA 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
